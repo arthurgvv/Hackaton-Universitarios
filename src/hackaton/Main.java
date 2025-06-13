ec7d9f6b0047f3c2a6a2fb7fea87c8ec480e7e5a
Matheus Guilherme <104092352+theuzao@users.noreply.github.com>
2025-06-13 14:45:52 -0300

    Update Main.java

--- src/hackaton/Main.java
+++ src/hackaton/Main.java
@@ -1,10 +1,8 @@
 package hackaton;
 
 import hackaton.model.*;
-import hackaton.BancaAvaliadora;
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
